2aa8163dcfd97840715039373bff52dbf6405081
OlgaPavlyuk <ou.pavlyuk@gmail.com>
2019-11-08 21:40:20 +0300

    Updates

---
+++ precache-manifest.ad93319567b4acb61dd751fa1ca6d35a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77467386f75050e4e66b7bbc718098d",
+    "url": "/react-learning/index.html"
+  },
+  {
+    "revision": "50c5a8f78ca90aac1362",
+    "url": "/react-learning/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7cc4fed633825e1271ab",
+    "url": "/react-learning/static/js/2.7476196b.chunk.js"
+  },
+  {
+    "revision": "50c5a8f78ca90aac1362",
+    "url": "/react-learning/static/js/main.93db764e.chunk.js"
+  },
+  {
+    "revision": "92912a7ad8bc48c53c5c",
+    "url": "/react-learning/static/js/runtime-main.a837d129.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-learning/static/media/logo.25bf045c.svg"
+  }
+]);
