e59cb15024626ad87f801fb60047a0063490de55
OlgaPavlyuk <ou.pavlyuk@gmail.com>
2019-12-04 14:15:00 +0300

    add color switcher

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
@@ -9,10 +9,17 @@ import MainContent from './Components/MainContent';
 import Elements from './Components/Elements';
 
 function App() {
+  /* todo: the theme changing in a proper way  */
+  const [ theme, setTheme ] = useState('pink');
+
+  const changeTheme = (newTheme) => {
+    return setTheme(newTheme);
+  }
+
   return (
     <Router>
-      <div className="app purple">
-        <Header />
+      <div className={`app ${theme}`}>
+        <Header theme={theme} onChangeTheme={changeTheme} />
         <main>
           <section className="content">
             <div className="container">
